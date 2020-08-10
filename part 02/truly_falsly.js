782abc9578acf2ede8fbcfe5dd1234774b79a9dc
Jannat-Ara <jannat.raya@northsouth.edu>
2020-08-10 22:08:40 +0600

    first

---
+++ part 02/truly_falsly.js
@@ -0,0 +1,16 @@
+const age=4;
+if(age>0){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+
+const age1=0;
+if(age1>0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
